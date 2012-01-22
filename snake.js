61fd6f8028656ee18b3f7c2291bb6301b0463ac0
conzett <conzett@gmail.com>
2012-01-21 23:26:35 -0500

    Pass JSLint

--- snake.js
+++ snake.js
@@ -1,4 +1,4 @@
-var snakeObject = function (direction, body) {
+var Snake = function (direction, body) {
 	'use strict';
 	this.body = body || [ { x: 10, y: 5}, { x: 10, y: 6}, { x: 10, y: 7}, { x: 10, y: 8} ];
 	this.direction = direction || 1;
@@ -73,46 +73,51 @@ var snakeObject = function (direction, body) {
 	};
 };
 
-var levelObject = function (snake, width, height, increment) {
+var Level = function (snake, width, height, increment) {
 	'use strict';
-	this.snake = snake || new snakeObject();
+	this.snake = snake || new Snake();
 	this.width = width || 300;
 	this.height = height || 170;
 	this.increment = increment || 10;
-	this.food = new Array();
-	
+	this.food = [];
+
 	this.createFood = function (x, y) {
 		this.food.push({x: x, y: y});
 	};
-	
+
 	this.outOfBounds = function () {
-		if(
-		   ((this.snake.body[0].x * this.increment >= this.width) ||
-		   (this.snake.body[0].x * this.increment < 0)) ||
-		   ((this.snake.body[0].y * this.increment >= this.height) ||
-		   (this.snake.body[0].y * this.increment < 0))
-		  ){
+		if (
+			(
+				(this.snake.body[0].x * this.increment >= this.width) ||
+				(this.snake.body[0].x * this.increment < 0)
+			) ||
+				(
+					(this.snake.body[0].y * this.increment >= this.height) ||
+					(this.snake.body[0].y * this.increment < 0)
+				)
+		) {
 			return true;
 		}
 		return false;
 	};
-}
+};
 
-var game = function (canvas, level, score, time) {
-	
+var Game = function (canvas, level, score, time) {
+	'use strict';
 	this.canvas = canvas || document.getElementById('level');
-	this.level = level || new levelObject();
+	this.level = level || new Level();
 	this.canvas.width = this.level.width;
 	this.canvas.height = this.level.height;
 	this.score = score || 0;
 	this.time = time || 100;
 	this.status = "play";
-	var that = this;
-	
+	var that = this,
+		context;
+
 	this.level.createFood(5, 8);
-	
-	window.addEventListener('keydown', function(event) {
-		
+
+	window.addEventListener('keydown', function (event) {
+
 		switch (event.keyCode) {
 		case 37:
 			that.level.snake.left();
@@ -127,60 +132,59 @@ var game = function (canvas, level, score, time) {
 			that.level.snake.down();
 			break;
 		}
-		
-	}, false );
-	
+
+	}, false);
+
 	this.loop = function () {
-	
+
 		context.clearRect(0, 0, that.level.width, that.level.height);
 		that.level.snake.move();
-	
+
 		var j;
-		
-		for(j=0; j< that.level.snake.body.length; j++)
-		{
+
+		for (j = 0; j < that.level.snake.body.length; j += 1) {
 			context.fillStyle = "rgb(0, 31, 0)";
 			context.fillRect(
 				that.level.snake.body[j].x * that.level.increment,
 				that.level.snake.body[j].y * that.level.increment,
-				that.level.increment, that.level.increment
+				that.level.increment,
+				that.level.increment
 			);
 		}
-		
-		if(that.level.food.length > 0){
+
+		if (that.level.food.length > 0) {
 			context.fillStyle = "rgb(139, 139, 109)";
 			context.fillRect(
 				that.level.food[0].x * that.level.increment,
 				that.level.food[0].y * that.level.increment,
-				that.level.increment, that.level.increment
+				that.level.increment,
+				that.level.increment
 			);
 		}
-		
-		if(that.level.outOfBounds() || that.level.snake.checkIntersection()){
+
+		if (that.level.outOfBounds() || that.level.snake.checkIntersection()) {
 			that.status = "stop";
 			document.getElementById('status').innerHTML = "You Lost";
 		}
-		
+
 		if ((that.level.food[0].x === that.level.snake.body[0].x) && (that.level.food[0].y === that.level.snake.body[0].y)) {
 			that.level.food.shift();
 			that.level.snake.grow();
 			that.level.createFood(Math.floor(Math.random() * (that.level.width / that.level.increment)), Math.floor(Math.random() * (that.level.height / that.level.increment)));
 			that.score += 10;
 		}
-		
-		document.getElementById('score').innerHTML = that.score;
-		
-		if ( that.status !== "stop" ) {			
-			setTimeout ( that.loop, that.time );
-		}
-	}
-	
-	if (this.canvas.getContext) {
-		var context = this.canvas.getContext('2d');
-		this.loop();
-	} else {
-		alert("Canvas Not Supported");
-	}
-}
 
-var game = new game();
+		document.getElementById('score').innerHTML = that.score;
+
+		if (that.status !== "stop") {
+			setTimeout(that.loop, that.time);
+		}
+	};
+
+	if (this.canvas.getContext) {
+		context = this.canvas.getContext('2d');
+		this.loop();
+	}
+};
+
+var game = new Game();
